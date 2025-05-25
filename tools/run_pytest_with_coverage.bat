6a5422dc886deb2ec8975f6a1a0b498e2fc24eac
FMProducer <sspostbox1@gmail.com>
2025-05-25 13:30:31 +0200

    250525 1308

--- tools/run_pytest_with_coverage.bat
+++ tools/run_pytest_with_coverage.bat
@@ -1,4 +1,4 @@
 @echo off
 cd /d %~dp0
-%~dp0.venv\Scripts\python.exe -m pytest -q --cov=adaptive_agent.exchange_api --cov-fail-under=90
+%~dp0.venv\Scripts\python.exe -m pytest -q --cov=exchange_api --cov=rebalance_engine --cov-fail-under=90
 pause
