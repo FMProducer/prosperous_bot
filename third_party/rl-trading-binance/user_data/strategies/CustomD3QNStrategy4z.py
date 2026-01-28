059ec0b700a1c0556e2647fa34760a38112a0348
FMProducer <sspostbox1@gmail.com>
2026-01-29 01:18:46 +0200

    docs: config_rl4z.json fix

--- third_party/rl-trading-binance/user_data/strategies/CustomD3QNStrategy4z.py
+++ third_party/rl-trading-binance/user_data/strategies/CustomD3QNStrategy4z.py
@@ -142,7 +142,7 @@ class CustomD3QNStrategy4z(IStrategy):
         self.long_1_model_pth = self.long_1_model_dir / "best.pth"
         
         # Long Model 2: A2C mean-reversion (используем ту же модель для примера, замените на вашу вторую)
-        self.long_2_model_dir = self.project_root / "output/alpha_seed_404_ohlcv_z_LONG_ONLY/saved_models/rl_binance_futures_trading_date_20260128_time_221802"
+        self.long_2_model_dir = self.project_root / "output/alpha_seed_404_ohlcv_z_LONG_ONLY/saved_models/rl_binance_futures_trading_date_20260129_time_002257"
         self.long_2_model_pth = self.long_2_model_dir / "best.pth"
         
         # Short Model 1: SAC bearish trending
