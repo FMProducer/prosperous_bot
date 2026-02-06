ae3d93242669d7cc81ee3465797bd4c9125e86d8
FMProducer <sspostbox1@gmail.com>
2026-02-06 21:36:24 +0200

    docs: changes models CustomD3QNStrategy4z.py

--- third_party/rl-trading-binance/user_data/strategies/CustomD3QNStrategy4z.py
+++ third_party/rl-trading-binance/user_data/strategies/CustomD3QNStrategy4z.py
@@ -184,11 +184,11 @@ class CustomD3QNStrategy4z(IStrategy):
 
         # --- ПУТИ К 4 МОДЕЛЯМ ---
         # Long Model 1:
-        self.long_1_model_dir = self.project_root / "output/alpha_seed_404_ohlcv_z_LONG_ONLY/saved_models/rl_binance_futures_trading_date_20260201_time_024250"
+        self.long_1_model_dir = self.project_root / "output/alpha_seed_404_ohlcv_z_LONG_ONLY/saved_models/rl_binance_futures_trading_date_20260131_time_235038"
         self.long_1_model_pth = self.long_1_model_dir / "best.pth"
         
         # Long Model 2:
-        self.long_2_model_dir = self.project_root / "output/alpha_seed_404_ohlcv_z_LONG_ONLY/saved_models/rl_binance_futures_trading_date_20260131_time_235038"
+        self.long_2_model_dir = self.project_root / "output/alpha_seed_404_ohlcv_z_LONG_ONLY/saved_models/rl_binance_futures_trading_date_20260125_time_033653"
         self.long_2_model_pth = self.long_2_model_dir / "best.pth"
         
         # Short Model 1:
@@ -196,7 +196,7 @@ class CustomD3QNStrategy4z(IStrategy):
         self.short_1_model_pth = self.short_1_model_dir / "best.pth"
         
         # Short Model 2:
-        self.short_2_model_dir = self.project_root / "output/alpha_seed_404_ohlcv_z_SHORT_ONLY/saved_models/rl_binance_futures_trading_date_20260201_time_232020"
+        self.short_2_model_dir = self.project_root / "output/alpha_seed_404_ohlcv_z_SHORT_ONLY/saved_models/rl_binance_futures_trading_date_20260126_time_214322"
         self.short_2_model_pth = self.short_2_model_dir / "best.pth"
         
         # --- ВКЛЮЧЕНИЕ/ОТКЛЮЧЕНИЕ МОДЕЛЕЙ ---
